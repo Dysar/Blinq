55d6773bfbef4ae21078356cb60db79d5f86845b
Mark Porohnja <maporo@ttu.ee>
2019-03-11 22:42:37 +0200

    minor updates

--- Blinq/ClientApp/src/app/app.module.ts
+++ Blinq/ClientApp/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { MonitoringComponent } from './monitoring/monitoring.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatInputModule, MatSelectModule, MatRadioModule, MatCardModule, MatTableModule, MatPaginatorModule, MatSortModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
-import { DataTableComponent } from './data-table/data-table.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from '../material-module';
+import { DataTableComponent } from './data-table/data-table.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
 
@@ -22,32 +23,20 @@ import { LayoutModule } from '@angular/cdk/layout';
     HomeComponent,
     MonitoringComponent,
     DataTableComponent,
-    SidenavComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
-    MatCheckboxModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'monitoring', component : MonitoringComponent}
     ]),
-    MatInputModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
     ReactiveFormsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
     LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
