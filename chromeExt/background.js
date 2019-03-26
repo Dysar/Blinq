4d35da4a74641128679ea468cb05c5797dee5afa
dazing <dazing@ttu.ee>
2019-03-26 18:36:50 +0200

    added Title to extension response

--- chromeExt/background.js
+++ chromeExt/background.js
@@ -1,14 +1,22 @@
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     // how to fetch tab url using activeInfo.tabid
     chrome.tabs.get(activeInfo.tabId, function(tab){
-        var data = JSON.stringify(tab.url);
+        
         if (String(tab.url) != "undefined") {
-        var xhr = new XMLHttpRequest();
-        var url = "http://localhost:4201/user-data";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(data);
-    }
+
+        let data = {URL: tab.url, Title: tab.title};
+        
+        var jsonData = JSON.stringify(data);
+            
+            var xhr = new XMLHttpRequest();
+            var url = "http://localhost:4201/user-data";
+            
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(jsonData);
+        }
     });
   }); 
+
+
 
--- chromeExt/popup.js
+++ chromeExt/popup.js
@@ -1,5 +1,15 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    console.log(url);
-    document.getElementById("myText").innerHTML = url;
-});
+// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//     var url = tabs[0].url;
+//     console.log(url);
+//     document.getElementById("myText").innerHTML = url;
+// });
+
+
+window.onload=function(){document.getElementById("myForm").addEventListener("submit", funcSubmit, false);  //Modern browsers
+}
+
+function funcSubmit() {
+  var usr = document.getElementById("myUsername").value;
+  var pwd = document.getElementById("myPassword").value;
+  alert(usr + pwd);
+}
