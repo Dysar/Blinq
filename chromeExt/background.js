add31fead80b89599f43e5f8c0dfcea4e9eeec59
dazing <dazing@ttu.ee>
2019-03-27 13:24:10 +0200

    getting e-mail from google-chrome using extension

--- chromeExt/background.js
+++ chromeExt/background.js
@@ -1,10 +1,13 @@
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     // how to fetch tab url using activeInfo.tabid
     chrome.tabs.get(activeInfo.tabId, function(tab){
-        
+       
+
+        chrome.identity.getProfileUserInfo(function(info) { email = info.email });
+
         if (String(tab.url) != "undefined") {
 
-        let data = {URL: tab.url, Title: tab.title};
+        let data = {URL: tab.url, Title: tab.title, Email: email};
         
         var jsonData = JSON.stringify(data);
             
