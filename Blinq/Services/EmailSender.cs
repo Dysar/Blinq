56837d20240981e0949dfccc6bed85913b00c436
Deniss-PC\Deniss <weave.warmane@gmail.com>
2019-04-08 14:30:37 +0300

    SendGrid Nunit package, EmailSender service.

---
+++ Blinq/Services/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Blinq.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly string _sendGridKey;
+        public EmailSender()
+        {
+            _sendGridKey = "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";
+        }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(_sendGridKey, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
