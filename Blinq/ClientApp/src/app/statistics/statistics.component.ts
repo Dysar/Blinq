7c6a3f137ff2ec92605e3ce765586c601e40a439
Artjom Lipodat <arlipo@ttu.ee>
2019-04-10 16:15:23 +0300

    chart angular and c# connected

--- Blinq/ClientApp/src/app/statistics/statistics.component.ts
+++ Blinq/ClientApp/src/app/statistics/statistics.component.ts
@@ -36,77 +36,42 @@ export class StatisticsComponent implements OnInit{
   );
 
   constructor(private _chart: ChartsService, private breakpointObserver: BreakpointObserver) {
-    this.chartData = {};
+    
   }
 
   ngOnInit() {
-    this.chartData = {
-      labels: ["Yotube", "Facebuk", "Other"],
-      datasets: [{
-        label: '# of Votes',
-        data: [50, 40, 10],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    };
-    //this._chart.dailyChart()
-    //  .subscribe(r => {
-    //    r.forEach(x => console.log(x));
-    //    let mainData = r.map(o => o.wastedTime);
-    //    let chartLabels = r.map(o => o.name);
-    //    console.log(mainData);
-    //    console.log(chartLabels);
-    //    //this.chart = new Chart('dailyPie',
-    //    //  {
-    //    //    type: 'pie',
-    //    //    data: {
-    //    //      datasets: [
-    //    //        {
-    //    //          data: mainData
-    //    //        }
-    //    //      ],
-    //    //      labels: chartLabels,
-    //    //    },
-    //    //    options: {
-    //    //      legend: {
-    //    //        display: true
-    //    //      },
-    //    //      cutoutPercentage: 0
-    //    //    }
-    //    //  });
-    //  });
-  }
-
-  public ngAfterViewInit() {
-    let chart = this.refChart.nativeElement;
-    let ctx = chart.getContext("2d");
-    let myChart = new Chart(ctx, {
-      type: 'pie',
-      data: this.chartData,
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
+    this._chart.dailyChart().subscribe(r => {
+      this.chartData = {
+        labels: r.map(o => o.name),
+        datasets: [{
+          label: '# of Votes',
+          data: r.map(o => o.wastedTime),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      };
+      let chart = this.refChart.nativeElement;
+      let ctx = chart.getContext("2d");
+      let myChart = new Chart(ctx, {
+        type: 'pie',
+        data: this.chartData,
+        options: {}
+      });
     });
   }
 }
