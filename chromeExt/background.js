1f241ee6e1d99791c49ecfee0c9ae2b7d723dc28
dazing <dazing@ttu.ee>
2019-04-12 15:16:57 +0300

    screen capturing

--- chromeExt/background.js
+++ chromeExt/background.js
@@ -1,15 +1,22 @@
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     // how to fetch tab url using activeInfo.tabid
+
+    //can use chrome.tabs.getCurrent(function callback)
     chrome.tabs.get(activeInfo.tabId, function(tab){
-       
 
         chrome.identity.getProfileUserInfo(function(info) { this.email = info.email });
 
         if (String(tab.url) != "undefined") {
         
+
+        let screenShot = chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, {format:"png"}, function (image) {
+            //alert(image);
+            return image; //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
+        });
+
         var ts = Math.round((new Date()).getTime() / 1000);
 
-        let data = {URL: tab.url, Title: tab.title, Email: this.email, Time: ts};
+        let data = {URL: tab.url, Title: tab.title, Email: this.email, Time: ts, ScreenShot: screenShot};
         
         var jsonData = JSON.stringify(data);
             
